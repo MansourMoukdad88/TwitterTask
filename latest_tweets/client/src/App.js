21008440762455d3f499d599d13926f63d06b15c
MansourMoukdad88 <m_sy@outlook.com>
2019-02-06 21:00:38 +0200

    Node Express server

---
+++ latest_tweets/client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './6IDq.gif';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">
+          <p>
+             <code>100 Tweets</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
